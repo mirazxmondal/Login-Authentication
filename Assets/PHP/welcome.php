280d38abbb5332890165cfa2703ffdf87113d3a3
miraz-mondal <mirazmondal.official@gmail.com>
2022-10-21 20:12:52 +0530

    Stable 1.0

--- Assets/PHP/welcome.php
+++ Assets/PHP/welcome.php
@@ -15,7 +15,7 @@ require 'session-check.php';
     </head>
     <body>
         <h1>Welcome</h1>
-        <h1 style="display:inline;"><?php echo($_SESSION['FIEM']['fname']);?></h1>
+        <h1 style="display:inline;"><?php echo($_SESSION['FIEM']['fname']);?></h1> &nbsp;
         <h1 style="display:inline;"><?php echo($_SESSION['FIEM']['lname']);?></h1> <br>
         <img src="<?php echo($_SESSION['FIEM']['furl']);?>" height="200px" width="200px"> <br>
         <h3><a href="./logout.php">Log out</a></h3> 
